87532c569a012c67e0ad9446d9145a86fe31ee93
Alan <alan-hd@hotmail.com>
2024-03-13 11:55:46 -0300

    Realizado segunda etapa do projeto

---
+++ src/produto/model/Bicicleta.java
@@ -0,0 +1,76 @@
+package produto.model;
+
+public  abstract class Bicicleta {
+
+	private String marca;
+	private String modelo;
+	private String tipo;
+	private String tamanhoQuadro;
+	private String cor;
+	private double preco;
+
+	public  Bicicleta(String marca, String modelo, String tipo, String tamanhoQuadro, String cor, double preco) {
+		
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipo = tipo;
+		this.tamanhoQuadro = tamanhoQuadro;
+		this.cor = cor;
+		this.preco = preco;
+		
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTamanhoQuadro() {
+		return tamanhoQuadro;
+	}
+
+	public void setTamanhoQuadro(String tamanhoQuadro) {
+		this.tamanhoQuadro = tamanhoQuadro;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+
+	
+
+	
+
+}
---
+++ src/produto/repository/BicicletaRepository.java
@@ -0,0 +1,17 @@
+package produto.repository;
+
+import produto.model.Bicicleta;
+
+public interface BicicletaRepository {
+
+	// Operações CRUD da Bicicleta
+
+	public void cadastrar(Bicicleta bicicleta);
+
+	public void listarTodas();
+
+	public void atualizar(Bicicleta bicicleta);
+
+	public void deletar(String modelo);
+
+}
